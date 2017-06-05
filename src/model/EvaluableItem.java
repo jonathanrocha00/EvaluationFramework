6d3c7e54b2858f6c43b61671fce892e299152da5
Jonathan Rocha <Jonathan Rocha@10.7.127.6>
2017-06-05 10:36:41 -0300

    Criteria injected into Evaluables.

--- src/model/EvaluableItem.java
+++ src/model/EvaluableItem.java
@@ -1,20 +1,37 @@
 package model;
 
-import java.util.List;
+import java.util.ArrayList;
 
 public class EvaluableItem extends FrameworkObject {
 	
 	// Attributes ====================================================
+	private ArrayList<ObjectiveCriterion> objectiveCriteriaToBeEvaluated;
+	private ArrayList<SubjectiveCriterion> subjectiveCriteriaToBeEvaluated;
 	
 	// Constructors ==================================================
-	public EvaluableItem(int id, String name, String description) {
+	public EvaluableItem(int id, String name, String description, 
+			ArrayList<ObjectiveCriterion> objectiveCriteriaToBeEvaluated, 
+			ArrayList<SubjectiveCriterion> subjectiveCriteriaToBeEvaluated) {
 		super(id, name, description);
+		
+		this.objectiveCriteriaToBeEvaluated = objectiveCriteriaToBeEvaluated;
+		this.subjectiveCriteriaToBeEvaluated = subjectiveCriteriaToBeEvaluated;
 	}
 	// Methods =======================================================
-	public List<Evaluation> getReceivedEvaluations() {
-		
-		// TODO
-		
-		return null;
+	public ArrayList<ObjectiveCriterion> getObjectiveCriteriaToBeEvaluated() {
+		return objectiveCriteriaToBeEvaluated;
 	}
+
+	public void setObjectiveCriteriaToBeEvaluated(ArrayList<ObjectiveCriterion> objectiveCriteriaToBeEvaluated) {
+		this.objectiveCriteriaToBeEvaluated = objectiveCriteriaToBeEvaluated;
+	}
+
+	public ArrayList<SubjectiveCriterion> getSubjectiveCriteriaToBeEvaluated() {
+		return subjectiveCriteriaToBeEvaluated;
+	}
+
+	public void setSubjectiveCriteriaToBeEvaluated(ArrayList<SubjectiveCriterion> subjectiveCriteriaToBeEvaluated) {
+		this.subjectiveCriteriaToBeEvaluated = subjectiveCriteriaToBeEvaluated;
+	}
+	
 }
